3d6d8f925022ac502c0a36ecec542a0b63fdb903
DEEPANSHI DIXIT <deepanshi.1224@gmail.com>
2019-11-12 18:32:11 +0530

    first commit

---
+++ sh.js
@@ -0,0 +1,12 @@
+Which famous British women murderer of the 19th century was never arrested?	
+On which mountain did Jesus take his last supper?	
+What is the largest number of five digits?	
+What is the most fractured human bone?	
+What is the most famous university of Paris?	
+Which animal is on the golden Flemish flag?	
+What is the name of the Indian holy river?	
+Which South American country is named after Venice?	
+How many stars feature on the flag of New Zealand?	
+What colour to do you get when you mix red and white?	
+What is both a French wine region and a luxury American automobile?	
+For which narrow sea strait is Hellespont the ancient name?	
